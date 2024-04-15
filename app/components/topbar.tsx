2ae03cc04105a4a9f8fa8241405e665469cffd52
ZaitonZz <philipcandelario1@gmail.com>
2024-04-15 22:11:00 +0800

    first commit

---
+++ app/components/topbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TopBar() {
+  return (
+    <div className='pl-20 flex items-center bg-[#558750] h-[9rem]'>
+        <img src='/brms_logo.png' className='w-1/3'></img>
+    </div>
+  )
+}
+
+export default TopBar
