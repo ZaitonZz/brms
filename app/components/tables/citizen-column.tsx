b857003bc7e7f21f1ed205f105b50f2217bc9cda
SadPotato <johncarlo.valleser@gmail.com>
2024-05-27 20:06:14 +0800

    trial run for PDF maker

--- app/components/tables/citizen-column.tsx
+++ app/components/tables/citizen-column.tsx
@@ -12,6 +12,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { PersonalInformation } from "../../types/types"
+import { download } from "@/components/tables"
  
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -111,7 +112,9 @@ export const CitizenColumns: ColumnDef<PersonalInformation>[] = [
               Delete Citizen
             </DropdownMenuItem>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>View Citizen</DropdownMenuItem>
+            <DropdownMenuItem 
+            onClick={() => download()}
+            >View Citizen</DropdownMenuItem>
             <DropdownMenuItem>View payment details</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
