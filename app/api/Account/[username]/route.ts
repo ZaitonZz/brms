9d11a44df45c4beca8e14ce2ff45a291a551051d
cycyqt <cyreljamestolentino14@gmail.com>
2024-05-28 04:03:00 +0800

    account update

--- app/api/Account/[username]/route.ts
+++ app/api/Account/[username]/route.ts
@@ -1,20 +1,20 @@
 import prisma from "@/prisma/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-
 export async function GET(req: NextRequest, { params }: { params: { username: string } }) {
     try {
         const account = await prisma.account.findFirst({
             where: {
-                Username: params.username
+                username: params.username
             }
         });
-        const accesslevel = account?.AccessLevel;
-        return NextResponse.json( accesslevel );
+        const accessLevel = account?.AccessLevel; 
+        return NextResponse.json(accessLevel);
     } catch (error) {
-        console.log('what', error);
+        console.log('Error:', error);
         return NextResponse.json({
-            status: 404
+            status: 404,
+            message: "Account not found"
         }); 
     }
 }
