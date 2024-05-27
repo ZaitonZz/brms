ad8ccb077fde14221389bd825a5613c31a15fb57
ZaitonZz <philipcandelario1@gmail.com>
2024-05-28 05:18:14 +0800

    fixed account route

--- app/api/Account/[username]/route.ts
+++ app/api/Account/[username]/route.ts
@@ -1,24 +1,20 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '@/prisma/prisma';
+import prisma from "@/prisma/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { username } = req.query;
-
-  try {
-    const account = await prisma.account.findFirst({
-      where: {
-        username: String(username)
-      }
-    });
-
-    if (account) {
-      const accessLevel = account?.AccessLevel; 
-      return res.status(200).json({ accessLevel });
-    } else {
-      return res.status(404).json({ status: 404, message: 'Account not found' });
+export async function GET(req: NextRequest, { params }: { params: { username: string } }) {
+    try {
+        const account = await prisma.account.findFirst({
+            where: {
+                Username: params.username
+            }
+        });
+        const accessLevel = account?.AccessLevel; 
+        return NextResponse.json(accessLevel);
+    } catch (error) {
+        console.log('Error:', error);
+        return NextResponse.json({
+            status: 404,
+            message: "Account not found"
+        }); 
     }
-  } catch (error) {
-    console.error('Error:', error);
-    return res.status(500).json({ status: 500, message: 'Internal Server Error' });
-  }
 }
