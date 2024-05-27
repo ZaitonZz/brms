be08d34d09329f3ad2188cc96606618a138cd04b
Stryxxe <cadgelicame00363@usep.edu.ph>
2024-05-27 16:48:28 +0800

    Citizen(part)

--- app/util/fetch-business-fees.ts
+++ app/util/fetch-business-fees.ts
@@ -3,7 +3,7 @@ import { businessFee } from "../types/types";
 
 const host = process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000';
 
-export async function fetchBusinessFee(): Promise<[businessFee]> {
+export async function fetchBusinessFee(): Promise<businessFee[]> {
     
   const res = await fetch(`${host}/api/fee/list/business/all`, {
     method: 'GET',
